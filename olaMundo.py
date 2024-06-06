7b9b99badaa4d79e72e050ef133d069edcde06a4
karlamramos <karla-a.k@outlook.com>
2024-06-06 14:13:09 -0300

    Add files via upload

---
+++ olaMundo.py
@@ -0,0 +1,7 @@
+nome = "Karla Maria"
+idade = "26"
+cidade = "Suzano - SP"
+formacao = "Python"
+
+print(f"Olá me chamo {nome} e tenho {idade} anos")
+print(f"Moro em {cidade} e estou atualmente estudando {formacao}.")
